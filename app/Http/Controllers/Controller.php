0f06cee47fe680b3f29579ab491a55415d777ae0
Fran <labrest03@gmail.es>
2018-05-30 06:03:04 +0200

    add validationErrorResponse for all controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,17 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+class Controller extends BaseController {
+
+    use AuthorizesRequests,
+        DispatchesJobs,
+        ValidatesRequests;
+
+    protected function validationErrorResponse(\Exception $e) {
+        return response()->json([
+                    'message' => $e->getMessage(),
+                    'errors' => $e->getErrors()->getMessages()
+                        ], 422);
+    }
+
 }
