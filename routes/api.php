3f7f571208f7d11f2e62c8832424e6c0a63f8dc8
Fran <labrest03@gmail.es>
2018-05-30 17:59:11 +0200

    add all routes as resources

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware'=>['auth:api']], function(){
+    Route::resource('providers', 'ProviderController');
+    Route::resource('products', 'ProductController');
+    Route::resource('orders', 'OrderController');
+    Route::resource('formulas', 'FormulaController');
+    Route::resource('category', 'CategoryController');
+    
+});
