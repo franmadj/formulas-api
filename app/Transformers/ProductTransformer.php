ab0c32f8b9c156409d246fd82e06ef9d86bbf2f7
Fran <labrest03@gmail.es>
2018-06-05 01:54:20 +0200

    format product output

--- app/Transformers/ProductTransformer.php
+++ app/Transformers/ProductTransformer.php
@@ -5,18 +5,24 @@ namespace App\Transformers;
 use App\Product;
 use Flugg\Responder\Transformers\Transformer;
 
-class ProductTransformer extends Transformer
-{
-
+class ProductTransformer extends Transformer {
 
     /**
      * Transform the model data into a generic array.
      *
-     * @param  Birthday $birthday
+     * @param  Product $product
      * @return array
      */
-    public function transform(Product $product):array
-    {
-        return $product->getAttributes();
+    public function transform(Product $product): array {
+        return [
+            "id" => $product->id,
+            "name" => $product->name,
+            "code" => $product->code,
+            "price" => $product->price,
+            "density" => $product->density,
+            "created_at" => $product->created_at->format('Y-m-d'),
+            "updated_at" => $product->updated_at->format('Y-m-d'),
+        ];
     }
+
 }
